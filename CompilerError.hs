ff315edf8b9de8a4731d31f6dd353078aa9ed3a4
Ingemar Ådahl <ingemar.adahl@gmail.com>
2012-10-03 14:46:30 +0200

    CErrorr available as a monad transformer..

--- CompilerError.hs
+++ CompilerError.hs
@@ -1,9 +1,11 @@
-{-# LANGUAGE UnicodeSyntax, MultiParamTypeClasses #-}
+{-# LANGUAGE UnicodeSyntax, MultiParamTypeClasses  #-}
 
 module CompilerError where
 
 import Control.Applicative
 import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Identity
 
 import Control.Monad.Error
 
@@ -20,29 +22,41 @@ instance Error CompilerError where
   noMsg    = UnknownError "Unknown error"
   strMsg s = UnknownError s
 
-data EitherError a = Pass a | Fail CompilerError
+data CError a = Pass a | Fail CompilerError
   deriving (Read, Show, Eq, Ord)
 
-instance Functor EitherError where
+newtype CErrorT m a = CErrorT { runCErrorT ∷ m (CError a) }
+
+instance Monad m ⇒ Monad (CErrorT m) where
+  return = (CErrorT . return . Pass)
+  CErrorT v >>= f = CErrorT $ do
+    result ← v
+    case result of
+      Pass a → runCErrorT $ f a
+      Fail e → return $ Fail e
+
+instance MonadTrans CErrorT where
+  lift m = CErrorT $ do
+    a ← m
+    return $ Pass a
+
+instance Functor CError where
   fmap f (Pass a) = Pass (f a)
   fmap _ (Fail e) = Fail e
 
-instance Applicative EitherError where
+instance Applicative CError where
   pure = Pass
   (Pass f) <*> a = fmap f a
   (Fail e) <*> _ = Fail e
 
-instance Monad EitherError where
+instance Monad CError where
   return       = Pass
   fail s       = Fail $ UnknownError s
   Pass a >>= f = f a
   Fail e >>= f = Fail e
 
-instance MonadError CompilerError EitherError where
+instance MonadError CompilerError CError where
     throwError             = Fail
     Pass  a `catchError` _ = Pass a
     Fail  f `catchError` h = h f
-
--- CompilerError alias
-type CErr = EitherError
 
