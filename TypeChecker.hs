39e88c9d0e9516d1bc4df21870f7bda91d998980
Ingemar Ådahl <ingemar.adahl@gmail.com>
2012-10-10 20:30:56 +0200

    Show instance for environment

--- TypeChecker.hs
+++ TypeChecker.hs
@@ -31,16 +31,14 @@ absError e p s = do
   throwError $ e p f s
 
 -- | Type of functions, where first value is return type, second is arguments
-type Function = (Type, [Type])
-
-returnType ∷ Function → Type
-returnType = fst
-
-arguments ∷ Function → [Type]
-arguments = snd
+type Function = (FilePath, Position, Type, [Type])
+type Variable = (FilePath, Position, Type)
 
 showFunction ∷ Function → String
-showFunction (ret, args) = intercalate " -> " $ map show (args ++ [ret])
+showFunction (_, _, ret, args) = intercalate " -> " $ map show (args ++ [ret])
+
+exists ∷ Function → [Function] → Bool
+(_,_,t,ts) `exists` fs = any (\(_,_,t',ts') → (t,ts) == (t',ts')) fs
 
 -- | Environment during type checking
 data Environment = Env {
@@ -48,13 +46,57 @@ data Environment = Env {
   options   ∷ Options,
   currentFile ∷ FilePath
 }
- deriving (Show)
+
+instance Show Environment where
+ show env = printf "Functions: %s\nVariables:%s" (showFuns (scopes env)) (showVars (scopes env))
+
+showVars ∷ [Scope] → String
+showVars scope = showVars' scope 2
+ where
+  showVars' ∷ [Scope] → Int → String
+  showVars' (s:sc) l = showVarsLevel (toList (variables s)) l ++ showVars' sc (l+2)
+  showVars' [] _ = ""
+
+showVarsLevel ∷ [(String,Variable)] → Int → String
+showVarsLevel vars l = newline ++ (intercalate newline $ map (uncurry showVar) vars)
+ where
+  newline = '\n':replicate l ' '
+
+showVar ∷ String → Variable → String
+showVar n (_,_,t) = printf "%s: %s" n $ show t
+
+showFuns ∷ [Scope] → String
+showFuns scope = showFuns' scope 2
+ where
+  showFuns' ∷ [Scope] → Int → String
+  showFuns' (s:sc) l = showFunsLevel (functions s) l ++ showFuns' sc (l+2)
+  showFuns' [] _ = ""
+
+showFunsLevel ∷ Map String [Function] → Int → String
+showFunsLevel funMap l = foldrWithKey reducer "" funMap
+ where
+  reducer n fs p = p ++ newline ++ intercalate newline (map (showFun n) fs)
+  newline = '\n':replicate l ' '
+
+--apa = (\n fs p → p ++ "\n" ++ intercalate "\n" (map (showFun n) fs))
+
+showFun ∷ String → Function → String
+showFun n f = printf "%s :: %s "n (showFunction f)
+
+--showScopes ∷ [Scope] → Int → String
+--showScopes (s:sc) l = showFuns (toList $ functions s) ++ showScopes sc (l+2)
+-- where
+--  --showFun n f = printf "%s%s: %s" (replicate l ' ') n (showFunction f)
+--showScopes [] _ = ""
 
 data Scope = Scope {
   functions ∷ Map String [Function],
-  variables ∷ Map CIdent Type
+  variables ∷ Map String Variable
 }
  deriving (Show)
+
+emptyScope ∷ Scope
+emptyScope = Scope { functions = empty, variables = empty }
 
 -- | Add a function to a scope
 saddFunction ∷ String → Function → Scope → Scope
@@ -65,6 +107,11 @@ saddFunction n t s = s { functions = fs' }
           then adjust (t:) n fs
           else insert n [t] fs
 
+saddVariable ∷ String → Variable → Scope → Scope
+saddVariable n v s = s { variables = vs }
+ where
+  vs = insert n v $ variables s
+
 -- | Creates an empty environment based on a set of options
 buildEnv ∷ Options → Environment
 buildEnv opts = Env {
@@ -74,16 +121,31 @@ buildEnv opts = Env {
 }
 
 -- | Adds a function to the environment, and makes sure there are no duplicates
-addFunction ∷ CIdent → Function → TCM ()
-addFunction (CIdent (pos, name)) types = do
-  scs ← gets scopes
-  let a = lookup name (functions $ head scs) >>= (\fs → if types `elem` fs then Just fs; else Nothing)
+addFunction ∷ String → Function → TCM ()
+addFunction name fun@(_,pos,_,_) = do
+  (s:ss) ← gets scopes
+  let a = lookup name (functions s) >>= (\fs → if fun `exists` fs then Just fs; else Nothing)
   case a of
-    Just _ → typeError pos $ printf "function '%s' with type %s already defined" name (showFunction types)
-    Nothing → modify (\st → st { scopes = addToTopScope scs name types })
+    Just _ → typeError pos $ printf "function '%s' with type %s already defined" name (showFunction fun)
+    Nothing → modify (\st → st { scopes = saddFunction name fun s : ss })
+
+-- | Adds a variable to the current scope, making sure there are no duplicates
+addVariable ∷ String → Variable → TCM ()
+addVariable name v@(_,pos,_) = do
+  (s:ss) ← gets scopes
+  case lookup name (variables s) of
+    Just _ → typeError pos $ printf "variable '%s' already defined" name
+    Nothing → modify (\st → st { scopes = saddVariable name v s : ss } )
+
+lookupVar ∷ String → TCM Variable
+lookupVar name = gets scopes >>= lookupVar'
  where
-  addToTopScope ∷ [Scope] → String → Function → [Scope]
-  addToTopScope (s:ss) n t = saddFunction n t s:ss
+  lookupVar' ∷ [Scope] → TCM Variable
+  lookupVar'     [] = fail "apa" -- TODO
+  lookupVar' (s:ss) = case lookup name (variables s) of
+    Just v  → return v
+    Nothing → lookupVar' ss
+
 
 -- | Sets which file is currently checked
 updateFile ∷ FilePath → TCM ()
@@ -94,7 +156,9 @@ type TCM a = StateT Environment CError a
 
 -- | Only used during interpretation runs
 instance (Show a) => Show (TCM a) where
-  show s = show $ runStateT s $ buildEnv Options { inputFile = "DEBUG" }
+  show res = case runStateT res $ buildEnv Options { inputFile = "DEBUG" } of
+    Pass (v,s) → printf "%s\n%s" (show v) (show s)
+    Fail e → show e
 
 -- | Typechecks the given abstract source and annotates the syntax tree
 typeCheck ∷ Options → Tree (FilePath, AbsTree) → CError (Tree (FilePath, AbsTree))
@@ -103,22 +167,27 @@ typeCheck opts tree = evalStateT (checkTree tree) (buildEnv opts)
 checkTree ∷ Tree (FilePath, AbsTree) → TCM (Tree (FilePath, AbsTree))
 checkTree tree = do
   Traverse.mapM addFunctions tree
+  --Traverse.mapM addStructs tree
   Traverse.mapM checkFunctions tree
 
   return tree
 
 -- | TODO: Move this to some other module
 paramType ∷ Param → Type
-paramType (ConstParamDec t _) = t
-paramType (ConstParamDefault t _  _) = t
-paramType (ParamDec t _) = t
-paramType (ParamDefault t _ _) = t
+paramType (ConstParamDec _ t i) = idToType i t
+paramType (ParamDec t i) = idToType i t
+paramType (ParamDefault t i _) = idToType i t
+
+idToType ∷ Id → Type → Type
+idToType (IdEmptyArray _) t = TArray t
+idToType (IdArray _ _) t = TArray t
+idToType _ t = t
 
 -- | Adds function type definitions to the state
 addFunctions ∷ (FilePath, AbsTree) → TCM ()
 addFunctions (filename, AbsTree tree) = do
   updateFile filename
-  sequence_ [ addFunction name (ret, map paramType types) | Function ret name types _ ← tree ]
+  sequence_ [ addFunction name (filename, pos, ret, map paramType types) | Function ret (CIdent (pos,name)) types _ ← tree ]
 
 checkFunctions ∷ (FilePath, AbsTree) → TCM ()
 checkFunctions (filename, AbsTree tree) = do
@@ -127,7 +196,7 @@ checkFunctions (filename, AbsTree tree) = do
   return ()
 
 example ∷ AbsTree
-example = AbsTree [Import (TImport ((1,1),"import")) "struct.fl",Function TColor (CIdent ((3,7),"main")) [] [], Function TColor (CIdent ((3,7),"main")) [] [SDecl (Dec TInt (DefaultVars [Ident (CIdent ((5,7),"b"))] (EInt 0))),SFor (TFor ((6,3),"for")) [FDecl (Dec TInt (DefaultVars [Ident (CIdent ((6,12),"a"))] (EInt 0)))] [ELt (EVar (Ident (CIdent ((6,17),"i")))) (EInt 10)] [EPostInc (EVar (Ident (CIdent ((6,23),"i"))))] (SBlock [SExp (EPostInc (EVar (Ident (CIdent ((9,5),"b")))))]),SReturn (TReturn ((11,3),"return")) (EVar (Ident (CIdent ((11,10),"b"))))]]
+example = AbsTree [Import (TkImport ((1,1),"import")) "struct.fl", Function TColor (CIdent ((3,7),"main")) [] [SDecl (Dec TInt (DefaultVars [Ident (CIdent ((5,7),"b"))] (EInt 0))),SFor (TkFor ((6,3),"for")) [FDecl (Dec TInt (DefaultVars [Ident (CIdent ((6,12),"a"))] (EInt 0)))] [ELt (EVar (Ident (CIdent ((6,17),"i")))) (EInt 10)] [EPostInc (EVar (Ident (CIdent ((6,23),"i"))))] (SBlock [SExp (EPostInc (EVar (Ident (CIdent ((9,5),"b")))))]),SReturn (TkReturn ((11,3),"return")) (EVar (Ident (CIdent ((11,10),"b"))))]]
 
 checkFunction ∷ CIdent → Type → [Param] → [Stm] → TCM ()
 checkFunction (CIdent (pos, name)) ret args stms = do
