f63e91b7aae342344aa3d54dba9f9bc36b47c453
Filip Lundborg <filipl@opera.com>
2012-10-19 14:42:41 +0200

    Added GLSL grammar.
    build.sh now produces AbsGLSL.hs and PrintGLSL.hs, copies them to
    FrontEnd/ and removes unnecessary files.

--- build.sh
+++ build.sh
@@ -5,3 +5,10 @@ cat FrontEnd/Instances_head.hs > FrontEnd/Instances.hs
 awk '/^newtype/ && $2!="CFloat" { printf "instance Token %s where\n\
   tkpos (%s (p,_)) = p\n\
   tkident (%s (_,s)) = s\n\n", $2, $2, $2 }' FrontEnd/AbsGrammar.hs >> FrontEnd/Instances.hs
+
+# Build GLSL abstract tree and pretty printer
+bnfc -haskell -p GLSL GLSL.cf &&
+cp GLSL/AbsGLSL.hs FrontEnd/ &&
+cp GLSL/PrintGLSL.hs FrontEnd/ &&
+rm -r GLSL
+
