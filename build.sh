79b1d80e98885438d9981b5c4b096a915211e353
Filip Lundborg <filipl@opera.com>
2012-10-26 16:20:22 +0200

    Refined build.sh to replace bnfcs generated Makefile.
    build.sh stops executing if something goes wrong on the way.

--- build.sh
+++ build.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
-bnfc -m -haskell -p FrontEnd Grammar.cf
+echo "---------- Building Filter Language(?) Grammar"
+bnfc -haskell -p FrontEnd Grammar.cf &&
+# like the Makefile provided by bnfc -m - but without documentation
+#   and compilation of TestGrammar binary.
+happy -gca FrontEnd/ParGrammar.y &&
+alex -g FrontEnd/LexGrammar.x
 
-cat FrontEnd/Instances_head.hs > FrontEnd/Instances.hs
+if [ $? -ne 0 ]
+then
+  echo "Something fialed."
+  exit 1
+fi
+
+cat FrontEnd/Instances_head.hs > FrontEnd/Instances.hs &&
 awk '/^newtype/ && $2!="CFloat" { printf "instance Token %s where\n\
   tkpos (%s (p,_)) = p\n\
-  tkident (%s (_,s)) = s\n\n", $2, $2, $2 }' FrontEnd/AbsGrammar.hs >> FrontEnd/Instances.hs
+  tkident (%s (_,s)) = s\n\n", $2, $2, $2 }' FrontEnd/AbsGrammar.hs >> FrontEnd/Instances.hs &&
 
+echo &&
+echo &&
+echo "---------- Building GLSL Grammar" &&
 # Build GLSL abstract tree and pretty printer
 bnfc -haskell -p GLSL GLSL.cf &&
+#happy -gca GLSL/ParGLSL.y
 sed "s/GLSL\./FrontEnd./g" GLSL/AbsGLSL.hs > FrontEnd/AbsGLSL.hs &&
 sed "s/GLSL\./FrontEnd./g" GLSL/PrintGLSL.hs > FrontEnd/PrintGLSL.hs &&
 rm -r GLSL
