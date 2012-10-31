6a4ac43951aa457a3fc75c2b5ab8b79eb3c953de
Filip Lundborg <filipl@opera.com>
2012-10-31 16:00:10 +0100

    Folding over Exp and Stm.

--- Compiler/Utils.hs
+++ Compiler/Utils.hs
@@ -6,6 +6,7 @@ import FrontEnd.AbsGrammar
 
 import TypeChecker.Utils (cIdentToString, paramToString)
 
+import Control.Monad.Identity
 
 -- | Finds all declared (inner) functions.
 findInnerFuns ∷ [Stm] → [AbsFun]
@@ -26,6 +27,82 @@ declPostToName (DecAss cids _ _) = map cIdentToString cids
 -- DecFun is replaced with SFunDecl in the typechecker.
 --declPostToName (DecFun {}) = []
 
+-- Exp and Stm helpers {{{
+foldExpM ∷ Monad m => (a → Exp → m a) → a → Exp → m a
+foldExpM f p e@(EAss el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EAssAdd el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EAssSub el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EAssMul el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EAssDiv el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EAssMod el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EAssBWAnd el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EAssBWXOR el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EAssBWOR el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(ECond econd _ etrue _ efalse) = foldM f p [econd,etrue,efalse,e]
+foldExpM f p e@(EOR el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EXOR el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EAnd el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EBWOR el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EBWXOR el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EBWAnd el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EEqual el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(ENEqual el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(ELt el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EGt el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(ELEt el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EGEt el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EBWShiftLeft el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EBWShiftRight el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EAdd el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(ESub el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EMul el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EDiv el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(EMod el _ er) = foldM f p [el,er,e]
+foldExpM f p e@(ENeg _ ei) = foldM f p [ei,e]
+foldExpM f p e@(ENegSign _ ei) = foldM f p [ei,e]
+foldExpM f p e@(EComplement _ ei) = foldM f p [ei,e]
+foldExpM f p e@(EPos _ ei) = foldM f p [ei,e]
+foldExpM f p e@(EPreInc _ ei) = foldM f p [ei,e]
+foldExpM f p e@(EPreDec _ ei) = foldM f p [ei,e]
+foldExpM f p e@(EPostInc ei _) = foldM f p [ei,e]
+foldExpM f p e@(EPostDec ei _) = foldM f p [ei,e]
+foldExpM f p e@(EMember ei _) = foldM f p [ei,e]
+foldExpM f p e@(EMemberCall ei _ es) = foldM f p $ [ei,e] ++ es
+foldExpM f p e@(ECall _ es) = foldM f p (e:es)
+foldExpM f p e@(ETypeCall _ es) = foldM f p (e:es)
+foldExpM f p e@(EVar {}) = f p e
+foldExpM f p e@(EIndex _ ei) = foldM f p [ei,e]
+foldExpM f p e@(EFloat {}) = f p e
+foldExpM f p e@(EInt {}) = f p e
+foldExpM f p e@ETrue = f p e
+foldExpM f p e@EFalse = f p e
+
+foldExp ∷ (a → Exp → a) → a → Exp → a
+foldExp f p e = runIdentity (foldExpM (liftIdentity f) p e)
+
+foldStmM ∷ Monad m => (a → Stm → m a) → a → Stm → m a
+foldStmM f p s@(SDecl {}) = f p s
+foldStmM f p s@(SExp {}) = f p s
+foldStmM f p s@(SBlock iss) = foldM f p (s:iss)
+foldStmM f p s@(SWhile _ _ is) = foldM f p [is,s]
+foldStmM f p s@(SDoWhile _ is _ _) = foldM f p [is,s]
+foldStmM f p s@(SFor _ _ _ _ is) = foldM f p [is,s]
+foldStmM f p s@(SReturn {}) = f p s
+foldStmM f p s@(SVoidReturn {}) = f p s
+foldStmM f p s@(SIf _ _ is) = foldM f p [is,s]
+foldStmM f p s@(SIfElse _ _ istrue _ isfalse) = foldM f p [istrue,isfalse,s]
+foldStmM f p s@(SBreak {}) = f p s
+foldStmM f p s@(SContinue {}) = f p s
+foldStmM f p s@(SDiscard {}) = f p s
+foldStmM f p s@(SType _ is) = foldM f p [is,s]
+foldStmM f p s@(SFunDecl _ _ _ iss) = foldM f p (s:iss)
+
+foldStm ∷ (a → Stm → a) → a → Stm → a
+foldStm f p s = runIdentity (foldStmM (liftIdentity f) p s)
+
+liftIdentity ∷ (a → b → a) → a → b → Identity a
+liftIdentity f' p' s' = Identity (f' p' s')
+-- }}}
 -- Free Variables {{{
 type AbsFun = (String, [Param], [Stm])
 
@@ -66,51 +143,10 @@ filterBound bound = filter (not . (`elem` bound))
 
 -- | Find all referenced variables in an expression.
 expVars ∷ Exp → [String]
-expVars (EAss el _ er) = concatMap expVars [el, er]
-expVars (EAssAdd el _ er) = concatMap expVars [el, er]
-expVars (EAssSub el _ er) = concatMap expVars [el, er]
-expVars (EAssMul el _ er) = concatMap expVars [el, er]
-expVars (EAssDiv el _ er) = concatMap expVars [el, er]
-expVars (EAssMod el _ er) = concatMap expVars [el, er]
-expVars (EAssBWAnd el _ er) = concatMap expVars [el, er]
-expVars (EAssBWXOR el _ er) = concatMap expVars [el, er]
-expVars (EAssBWOR el _ er) = concatMap expVars [el, er]
-expVars (ECond ec _ et _ ef) = concatMap expVars [ec, et, ef]
-expVars (EOR el _ er) = concatMap expVars [el, er]
-expVars (EXOR el _ er) = concatMap expVars [el, er]
-expVars (EAnd el _ er) = concatMap expVars [el, er]
-expVars (EBWOR el _ er) = concatMap expVars [el, er]
-expVars (EBWXOR el _ er) = concatMap expVars [el, er]
-expVars (EBWAnd el _ er) = concatMap expVars [el, er]
-expVars (EEqual el _ er) = concatMap expVars [el, er]
-expVars (ENEqual el _ er) = concatMap expVars [el, er]
-expVars (ELt el _ er) = concatMap expVars [el, er]
-expVars (EGt el _ er) = concatMap expVars [el, er]
-expVars (ELEt el _ er) = concatMap expVars [el, er]
-expVars (EGEt el _ er) = concatMap expVars [el, er]
-expVars (EBWShiftLeft el _ er) = concatMap expVars [el, er]
-expVars (EBWShiftRight el _ er) = concatMap expVars [el, er]
-expVars (EAdd el _ er) = concatMap expVars [el, er]
-expVars (ESub el _ er) = concatMap expVars [el, er]
-expVars (EMul el _ er) = concatMap expVars [el, er]
-expVars (EDiv el _ er) = concatMap expVars [el, er]
-expVars (EMod el _ er) = concatMap expVars [el, er]
-expVars (ENeg _ e) = expVars e
-expVars (ENegSign _ e) = expVars e
-expVars (EComplement _ e) = expVars e
-expVars (EPos _ e) = expVars e
-expVars (EPreInc _ e) = expVars e
-expVars (EPreDec _ e) = expVars e
-expVars (EPostInc e _) = expVars e
-expVars (EPostDec e _) = expVars e
-expVars (EMember e _) = expVars e
-expVars (EMemberCall e _ es) = expVars e ++ concatMap expVars es
-expVars (ECall _ es) = concatMap expVars es
-expVars (ETypeCall _ es) = concatMap expVars es
-expVars (EVar cid) = [cIdentToString cid]
-expVars (EIndex cid e) = cIdentToString cid : expVars e
-expVars (EFloat {}) = []
-expVars (EInt {}) = []
-expVars (ETrue) = []
-expVars (EFalse) = []
+expVars = foldExp expVar []
+ where
+  expVar ∷ [String] → Exp → [String]
+  expVar p (EVar cid) = cIdentToString cid : p
+  expVar p (EIndex cid _) = cIdentToString cid : p
+  expVar p _ = p
 -- }}}
