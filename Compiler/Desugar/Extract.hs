ff9e2d8d1275af6350ab4a67e7296c237718252e
Ingemar Ådahl <ingemar.adahl@gmail.com>
2012-11-21 13:40:30 +0100

    Extracting of arbitrary partial applications

--- Compiler/Desugar/Extract.hs
+++ Compiler/Desugar/Extract.hs
@@ -4,30 +4,47 @@ module Compiler.Desugar.Extract  where
 
 import Control.Monad.State
 
-import qualified Data.Traversable as Map (mapM)
+import Data.Maybe
+import qualified Data.Map as Map
+import qualified Data.Traversable as Trav (mapM)
 
 import Compiler.Utils
+import TypeChecker.Utils
 
 import TypeChecker.Types
 import FrontEnd.AbsGrammar
 
+import Text.Printf
+
 data Ids = Ids {
   unused ∷ [Int],
-  pending ∷ [Stm]
+  pending ∷ [Stm],
+  funs ∷ Map.Map String Function
 }
 
 newVar ∷ State Ids Exp
 newVar = do
-  e ← gets (("_l" ++) . show . head . unused)
+  e ← gets (printf "_l%03d" . head . unused)
   modify (\st → st { unused = tail (unused st) })
   return $ EVar (CIdent ((0,0), e))
 
 putDec ∷ Stm → State Ids ()
 putDec stm = modify (\st → st { pending = pending st ++ [stm] })
 
+getPendings ∷ State Ids [Stm]
+getPendings = do
+  p ← gets pending
+  modify (\st → st { pending = []})
+  return p
+
+getFun ∷ String → State Ids Function
+getFun s = do
+  fs ← gets funs
+  return $ fromJust $ Map.lookup s fs
+
 
 expandSource ∷ Source → Source
-expandSource src = src { functions = evalState (Map.mapM expandFun (functions src)) (Ids [1..] [])}
+expandSource src = src { functions = evalState (Trav.mapM expandFun (functions src)) (Ids [1..] [] (functions src))}
 
 expandFun ∷ Function → State Ids Function
 expandFun fun = do
@@ -37,19 +54,19 @@ expandFun fun = do
 expand ∷ [Stm] → State Ids [Stm]
 expand (SExp e:ss) = do
   e' ← extractCalls e
-  sdecs ← gets pending
+  sdecs ← getPendings
   ss' ← expand ss
   return $ sdecs ++ [SExp e'] ++ ss'
 expand (SWhile tkw e stm:ss) = do
   e' ← extractCalls e
-  sdecs ← gets pending
+  sdecs ← getPendings
   stm' ← liftM makeBlock $ expand [stm]
   ss' ← expand ss
   return $ sdecs ++ [SWhile tkw e' stm'] ++ ss'
 expand (SDoWhile tkd stm tkw e:ss) = do
   stms ← expand [stm]
   e' ← extractCalls e
-  sdecs ← gets pending
+  sdecs ← getPendings
   let stm' = makeBlock $ stms ++ sdecs
   ss' ← expand ss
   return $ SDoWhile tkd stm' tkw e':ss'
@@ -57,7 +74,7 @@ expand (SDoWhile tkd stm tkw e:ss) = do
 --expand SIf|SIfElse..
 expand (SReturn tkr e:ss) = do
   e' ← extractCalls e
-  sdecs ← gets pending
+  sdecs ← getPendings
   ss' ← expand ss
   return $ sdecs ++ [SReturn tkr e'] ++ ss'
 expand ss = expandStmM expand ss
@@ -66,13 +83,14 @@ extractCalls ∷ Exp → State Ids Exp
 extractCalls (EPartCall cid es ts) = do
   es' ← mapM extractCalls es
   var@(EVar varCid) ← newVar
-  putDec $ makeDec varCid (EPartCall cid es' ts)
+  f ← getFun (cIdentToString cid)
+  let typ = TFun (retType f) $ map varType $ drop (length ts) (paramVars f)
+  putDec $ makeDec varCid typ (EPartCall cid es' ts)
   return var
 extractCalls e = mapExpM extractCalls e
 
-makeDec ∷ CIdent → Exp → Stm
-makeDec cid e = SDecl (Dec [QType imgtype]
+makeDec ∷ CIdent → Type → Exp → Stm
+makeDec cid typ e = SDecl (Dec [QType typ]
                          (DecAss [cid] tkass e))
  where
-  imgtype = TFun TVec4 [TFloat, TFloat]
   tkass = TkAss ((0,0), "=")
