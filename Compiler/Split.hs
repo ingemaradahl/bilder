49a456bbdac22d817fe61cfb24afc51079b816bc
Filip Lundborg <filipl@opera.com>
2012-11-28 14:32:45 +0100

    expDeps fully implemented.

--- Compiler/Split.hs
+++ Compiler/Split.hs
@@ -369,7 +369,7 @@ stmDeps (SBreak _) = []
 stmDeps (SContinue _) = []
 stmDeps (SDiscard _) = []
 stmDeps (SType _ s) = stmDeps s
-stmDeps (SFunDecl {}) = error "SFunDecl not allowed here."
+stmDeps (SFunDecl {}) = error "SFunDecl really not allowed here."
 
 forDeclDeps ∷ ForDecl → DepList
 forDeclDeps (FDecl d) = declDeps d
@@ -386,22 +386,88 @@ declPostDeps (DecAss cids _ e) =
 declPostDeps (DecFun {}) = error "inner function declarations not allowed."
 
 expDeps ∷ Exp → DepList
-expDeps (EAss (EVar cid) _ e) =
-  (Var (cIdentToString cid), concatMap snd e') : onlyAssDeps e'
+expDeps (EAss el _ er) = expAss el er
+expDeps (EAssAdd el _ er) = expAss el er
+expDeps (EAssSub el _ er) = expAss el er
+expDeps (EAssMul el _ er) = expAss el er
+expDeps (EAssDiv el _ er) = expAss el er
+expDeps (EAssMod el _ er) = expAss el er
+expDeps (EAssBWAnd el _ er) = expAss el er
+expDeps (EAssBWXOR el _ er) = expAss el er
+expDeps (EAssBWOR el _ er) = expAss el er
+expDeps (ECond ec _ et _ ef) = concatMap expDeps [ec,et,ef]
+expDeps (EOR el _ er) = concatMap expDeps [el,er]
+expDeps (EXOR el _ er) = concatMap expDeps [el,er]
+expDeps (EAnd el _ er) = concatMap expDeps [el,er]
+expDeps (EBWOR el _ er) = concatMap expDeps [el,er]
+expDeps (EBWXOR el _ er) = concatMap expDeps [el,er]
+expDeps (EBWAnd el _ er) = concatMap expDeps [el,er]
+expDeps (EEqual el _ er) = concatMap expDeps [el,er]
+expDeps (ENEqual el _ er) = concatMap expDeps [el,er]
+expDeps (ELt el _ er) = concatMap expDeps [el,er]
+expDeps (EGt el _ er) = concatMap expDeps [el,er]
+expDeps (ELEt el _ er) = concatMap expDeps [el,er]
+expDeps (EGEt el _ er) = concatMap expDeps [el,er]
+expDeps (EBWShiftLeft el _ er) = concatMap expDeps [el,er]
+expDeps (EBWShiftRight el _ er) = concatMap expDeps [el,er]
+expDeps (EAdd el _ er) = concatMap expDeps [el,er]
+expDeps (ESub el _ er) = concatMap expDeps [el,er]
+expDeps (EMul el _ er) = concatMap expDeps [el,er]
+expDeps (EDiv el _ er) = concatMap expDeps [el,er]
+expDeps (EMod el _ er) = concatMap expDeps [el,er]
+expDeps (ENeg _ e) = expDeps e
+expDeps (ENegSign _ e) = expDeps e
+expDeps (EComplement _ e) = expDeps e
+expDeps (EPos _ e) = expDeps e
+expDeps (EPreInc _ e) = expDeps e
+expDeps (EPreDec _ e) = expDeps e
+expDeps (EPostInc e _) = expDeps e
+expDeps (EPostDec e _) = expDeps e
+-- Only non "real" members (e.g. xyz).
+expDeps (EMember e _) = (None, concatMap snd e') : e'
  where
   e' = expDeps e
+-- Only built in member calls (e.g. map).
 expDeps (EMemberCall e _ es) =
   (None, concatMap snd es') : es'
  where
-  es' = expDeps e ++ concatMap expDeps es
+  es' = concatMap expDeps (e:es)
 expDeps (ECall cid es) = (None, Var (cIdentToString cid) : Fun (cIdentToString cid) : concatMap (concatMap snd) es') : concatMap onlyAssDeps es'
  where
   es' = map expDeps es
+expDeps (ETypeCall _ es) = concatMap expDeps es
 expDeps (EVar cid) = [(None, [Var (cIdentToString cid)])]
+expDeps (EIndex cid e) = (None, Var (cIdentToString cid) : concatMap snd e') : e'
+ where
+  e' = expDeps e
 expDeps (EFloat {}) = []
+expDeps (EInt _) = []
+expDeps (ETrue) = []
+expDeps (EFalse) = []
+expDeps (EPartCall cid es _) =
+  (Fun (cIdentToString cid), concatMap snd es') : es'
+ where
+  es' = concatMap expDeps es
+expDeps (ECurryCall {}) = error "ECurryCall should be uncurried here."
 expDeps (EVarType cid _) = [(None, [Var (cIdentToString cid)])]
 
-expDeps e = error $ "expDeps: not implemented " ++ show e
+expAss ∷ Exp → Exp → DepList
+expAss (EVar cid) e =
+  (Var (cIdentToString cid), concatMap snd (expDeps e)) : onlyAssDeps (expDeps e)
+expAss (EIndex cid e) el =
+  (Var (cIdentToString cid), concatMap snd (el'++e')) : onlyAssDeps el' ++ e'
+ where
+  e' = expDeps e
+  el' = expDeps el
+expAss el er =
+  -- el depends on all er's dependencies
+  map (\(a,c) → (a,c++desper)) el'
+  -- and the whole assignment depends on all dependencies in er'
+  ++ er'
+ where
+  el' = expDeps el
+  er' = expDeps er
+  desper = concatMap snd er'
 
 onlyAssDeps ∷ DepList → DepList
 onlyAssDeps = filter ((/=) None. fst)
