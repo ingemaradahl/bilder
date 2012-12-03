808e15cca2390721dfc4f0da45890da397c3ab47
Ingemar Ådahl <ingemar.adahl@gmail.com>
2012-12-03 15:03:45 +0100

    More extensible structure of compiler.

--- Compiler.hs
+++ Compiler.hs
@@ -4,21 +4,26 @@ module Compiler where
 
 import Control.Monad.State hiding (mapM)
 
+import Control.Arrow
+import qualified Data.Map as Map
+
+import TypeChecker.Types (Source)
+
+import Compiler.Simple.AbsSimple
+import Compiler.Simple.Types
+import Compiler.Simple.Utils
+import Compiler.GLSL.Utils
+
 import CompilerError
-import Compiler.Utils
+import Compiler.Utils (liftCError)
 
 import qualified Compiler.Lifter as L
 import Compiler.Desugar (desugar)
 import Compiler.Split (splitSource)
 import Compiler.Simple (absToSimple, simpleToGLSL)
 
--- TEMP
-import Compiler.GLSL.Utils
+import qualified FrontEnd.AbsGLSL as G
 
-
-import FrontEnd.AbsGLSL as G
-
-import TypeChecker.Types
 
 data Options = Options {
   inputFile ∷ FilePath
@@ -52,4 +57,34 @@ lambdaLift src = do
 
 compile ∷ Source → CPM [G.Tree]
 compile src =
-  liftM (simpleToGLSL . map finalizeMain . absToSimple .splitSource . desugar) $ lambdaLift src
+  liftM (
+        desugar
+    >>> splitSource       -- Split to [Shader] with AbsGrammar
+    >>> absToSimple       -- Convert to SimpleGrammar
+    >>> map finalizeMain  -- Replace main-function with GLSL-variant
+    >>> simpleToGLSL      -- Translate to GLSL
+  ) $ lambdaLift src
+
+
+
+
+finalizeMain ∷ Shader → Shader
+finalizeMain shd =
+  shd { functions = Map.adjust reworkMain "main" (functions shd) }
+
+reworkMain ∷ Function → Function
+reworkMain (Function name _ [x, y] stms) = Function name TVoid [] (d:stms')
+ where
+  p = Variable "p" TVec2 True
+  d = SDeclAss p (EDiv gl_FragCoord_xy fl_Resolution)
+  x' = EMember (EVar (variableName p)) "x"
+  y' = EMember (EVar (variableName p)) "y"
+  stms' = map (subReturn . mapStmExp subXY) stms
+  subXY ∷ Exp → Exp
+  subXY (EVar s) | s == variableName x = x'
+  subXY (EVar s) | s == variableName y = y'
+  subXY e = mapExp subXY e
+  subReturn ∷ Stm → Stm
+  subReturn (SReturn e) = SExp $ EAss gl_FragColor e
+  subReturn s = mapStm subReturn s
+
