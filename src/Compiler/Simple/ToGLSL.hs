08c76bdbfbfd5f344ba107cc285fb1c712837c7d
Ingemar Ådahl <ingemar.adahl@gmail.com>
2013-01-28 18:04:48 +0100

    Added licensing and author information

--- src/Compiler/Simple/ToGLSL.hs
+++ src/Compiler/Simple/ToGLSL.hs
@@ -1,3 +1,23 @@
+{-
+ -      This file is part of Bilder.
+ -
+ -   Bilder is free software: you can redistribute it and/or modify
+ -   it under the terms of the GNU Lesser General Public License as published by
+ -   the Free Software Foundation, either version 3 of the License, or
+ -   (at your option) any later version.
+ -
+ -   Bilder is distributed in the hope that it will be useful,
+ -   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ -   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ -   GNU Lesser General Public License for more details.
+ -
+ -   You should have received a copy of the GNU Lesser General Public License
+ -   along with Bilder.  If not, see <http://www.gnu.org/licenses/>.
+ -
+ -   Copyright © 2012-2013 Filip Lundborg
+ -   Copyright © 2012-2013 Ingemar Ådahl
+ -
+ -}
 {-# LANGUAGE UnicodeSyntax #-}
 
 module Compiler.Simple.ToGLSL where
